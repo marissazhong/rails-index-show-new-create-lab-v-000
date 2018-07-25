00f46d567076e55f9a51891c59d6bc4bdc684bbf
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 00:42:15 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,22 @@
 class CouponsController < ApplicationController
 
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
 
 end
